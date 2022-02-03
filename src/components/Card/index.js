fa81f62b3edd31a846010c6f330c2890a52db1db
LeonardoCesca <leonardocescaf@gmail.com>
2022-02-03 08:37:55 -0300

    add: Link on detail button into Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,15 +3,18 @@ import PropTypes from 'prop-types';
 import './style.scss';
 import { AiFillEdit } from 'react-icons/ai';
 import { BsInfoSquare } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
-const Card = ({name, thumbnail}) => {
+const Card = ({name, thumbnail, id, routeDetail}) => {
     return (
         <div className='card'>
             <img src={`${thumbnail.path}.${thumbnail.extension}`} alt={name} className='card__image' />
             <p className='card__title'>{name}</p>
             <div className='card__icons'>
                 <AiFillEdit />
-                <BsInfoSquare />
+                <Link to={`${routeDetail}/${id}`}>
+                    <BsInfoSquare />
+                </Link>
             </div>
         </div>
     );
@@ -19,7 +22,9 @@ const Card = ({name, thumbnail}) => {
 
 Card.propTypes = {
     name: PropTypes.string.isRequired,
-    thumbnail: PropTypes.object.isRequired
+    routeDetail: PropTypes.string.isRequired,
+    thumbnail: PropTypes.object.isRequired,
+    id: PropTypes.number.isRequired
 };
 
 export default Card;
