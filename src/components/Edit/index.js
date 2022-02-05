c1450326ecdc6dc4fa83b0698cc80d1e6c96b31e
LeonardoCesca <leonardocescaf@gmail.com>
2022-02-04 22:51:03 -0300

    add: Form component and update Edit component

--- src/components/Edit/index.js
+++ src/components/Edit/index.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import './style.scss';
-import { useDispatch } from 'react-redux';
-import { returnUpdateCharacter } from '../../redux/actions/characters';
 
 import { AiFillEdit } from 'react-icons/ai';
+import Form from '../Form';
 
 const Edit = ({selectedCard}) => {
-    const dispatch = useDispatch();
     const [modal, setModal] = React.useState(false);
-    const [name, setName] = React.useState('');
 
     const handleEdit = () => {
         setModal(true);
-        setName(selectedCard.name);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(returnUpdateCharacter(selectedCard.id, name));
-        setModal(false);
     };
 
     return (
@@ -30,25 +20,7 @@ const Edit = ({selectedCard}) => {
             />
 
             {modal && (
-                <form 
-                    className='form'
-                    onSubmit={(e) => handleSubmit(e)}
-                >
-                    <h3 className='form__title'>Editar</h3>
-                    <label htmlFor={name} className='form__label'>Nome</label>
-                    <input 
-                        id={name}
-                        type="text" 
-                        value={name}
-                        className='form__input'
-                        onChange={(e) => setName(e.target.value)}
-                        />
-                    <input 
-                        className='form__btn'
-                        type="submit" 
-                        value="Enviar" 
-                    />
-                </form> 
+                <Form infos={selectedCard} />
             )}
         </>
     );
