aa2ef8d38cde76f644e51ea4d04288d2547a15d4
LeonardoCesca <leonardocescaf@gmail.com>
2022-02-05 09:47:21 -0300

    fix: Edit component

--- src/components/Edit/index.js
+++ src/components/Edit/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './style.scss';
 
 import { AiFillEdit } from 'react-icons/ai';
-import Form from '../Form';
+import { useDispatch } from 'react-redux';
+import { returnUpdateCharacter } from '../../redux/actions/characters';
 
 const Edit = ({selectedCard}) => {
+    const dispatch = useDispatch();
+    const [name, setName] = React.useState('');
     const [modal, setModal] = React.useState(false);
 
     const handleEdit = () => {
+        setName(selectedCard.name);
         setModal(true);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(returnUpdateCharacter(selectedCard.id, name));
+        setModal(false);
     };
 
     return (
@@ -20,7 +30,25 @@ const Edit = ({selectedCard}) => {
             />
 
             {modal && (
-                <Form infos={selectedCard} />
+                <form 
+                className='form'
+                onSubmit={(e) => handleSubmit(e)}
+                >
+                    <h3 className='form__title'>Editar</h3>
+                    <label htmlFor={name} className='form__label'>Nome</label>
+                    <input 
+                        id={name}
+                        type="text" 
+                        value={name}
+                        className='form__input'
+                        onChange={(e) => setName(e.target.value)}
+                        />
+                    <input 
+                        className='form__btn'
+                        type="submit" 
+                        value="Enviar" 
+                    />
+                </form> 
             )}
         </>
     );
